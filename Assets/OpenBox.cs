fd7aca8ac9c6f856cac1764294f8f41b026276b7
Ali Nategh <a.nategh76@gmail.com>
2020-12-27 18:23:20 +0330

    Second commit

---
+++ Assets/OpenBox.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBox : MonoBehaviour
+{
+    private bool boxOpened;
+    private bool coroutineAllowed;
+    private Vector3 initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxOpened = false;
+        coroutineAllowed = true;
+        initialPosition = transform.position;
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if(coroutineAllowed) Invoke("RunCoroutine", 0f);
+    }
+
+    private void RunCoroutine()
+    {
+        StartCoroutine("OpenThatDoor");
+    }
+    private IEnumerator OpenThatDoor()
+    {
+        coroutineAllowed = false;
+        if(!boxOpened)
+        {
+            for(float i = 0f; i<=2f; i += 0.1f)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, 
+                    transform.localPosition.y, transform.localPosition.z + 0.1f);
+                yield return new WaitForSeconds(0.05f);
+            }
+            boxOpened = true;
+        }
+        else
+        {
+            for(float i = 2f; i >= 0f; i -= 0.1f)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x,
+                    transform.localPosition.y, transform.localPosition.z - 0.1f);
+                yield return new WaitForSeconds(0.05f);
+            }
+            transform.position = initialPosition;
+            boxOpened = false;
+        }
+        coroutineAllowed = true;
+    }
+}
---
+++ Assets/Wikitude/Samples/Scripts/InstantTracking/DockUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/* Script that scales the dock used in the Instant Tracking samples based on the aspect ratio. */
+public class DockUI : MonoBehaviour
+{
+    public RectTransform ButtonsParent;
+
+    private void Awake() {
+        Update();
+    }
+
+    private void Update() {
+        float currentRatio = (float)Screen.width / (float)Screen.height;
+        if (currentRatio > 1.0f) {
+            ButtonsParent.sizeDelta = new Vector2(-600f, 0f);
+        } else {
+            ButtonsParent.sizeDelta = new Vector2(0f, 0f);
+        }
+    }
+}
